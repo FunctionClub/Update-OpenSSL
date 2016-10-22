0b3a03dc896b0f1ba717421f703b1b0235d5df53
Kirito <cxh20000210@qq.com>
2016-10-22 15:13:06 +0800

    Cli mode

--- openssl.sh
+++ openssl.sh
@@ -1,19 +1,3 @@
-
-
-
-
-
-
-
-#!/bin/bash
-
-whiptail --title "openssl Update Tool" --msgbox "This tool will update openssl to latest 1.0.2,Click OK." 10 60
-whiptail --title "Author" --msgbox "Author: Kirito ,Blog: https://ixh.me/ , Click OK." 10 60
-#!/bin/bash
-
-if (whiptail --title "Tos" --yesno "No support, No promise, advice on flesh centos system" 10 60) then
-
-if (whiptail --title "Update Gcc and install chacha 20?" --yesno "Update GCC to 4.8 and install Chacha20？" 10 60) then
 mkdir Openssl
 cd Openssl
 echo "Install basic Toolkit"
@@ -53,37 +37,3 @@ echo "Install openSSL"
 ./config --prefix=/usr shared zlib-dynamic
 make depend
 make && make install
-openssl version
-else
-mkdir Openssl
-cd Openssl
-echo "Install basic Toolkit"
-yum -y install pcre-devel zlib unzip git patch gcc gcc-c++
-echo "Install Zlib"
-wget http://zlib.net/zlib-1.2.8.tar.gz
-tar -zxf zlib-1.2.8.tar.gz
-cd zlib*
-./configure
-make test
-make install
-make clean
-./configure --shared
-make test
-make install
-cp zutil.h /usr/local/include
-cp zutil.c /usr/local/include
-cd ../
-echo "Download openSSL"
-wget https://www.openssl.org/source/openssl-1.0.2-latest.tar.gz
-tar -zxf openssl-1.0.2-latest.tar.gz
-echo "Move to openSSL"
-cd openssl-1.0.2*
-echo "Install openSSL"
-./config --prefix=/usr shared zlib-dynamic
-make depend
-make && make install
-openssl version
-fi
-else
-fi
-
